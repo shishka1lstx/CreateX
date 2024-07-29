fdbdc66edad4762ef898277e0288e02bb7cecad3
shishka <benz.hokobyan@gmail.com>
2024-07-29 15:45:26 +0300

    Add files via upload

---
+++ scripts/mixScript.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    var mixer = mixitup('.directions__list');
+
+    $('.directions__btn').on('click', function(){
+        $('.directions__btn').removeClass('active')
+        $(this).addClass('active')
+    })
+})
