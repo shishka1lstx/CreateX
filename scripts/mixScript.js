b56854b7da444839aae4910812b092c7ca3ae7bc
shishka <benz.hokobyan@gmail.com>
2024-07-31 03:38:36 +0300

    Add files via upload

--- scripts/mixScript.js
+++ scripts/mixScript.js
@@ -7,3 +7,5 @@ $(function(){
         $(this).addClass('active')
     })
 })
+
+
---
+++ scripts/teamslider.js
@@ -0,0 +1,10 @@
+$('.team__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: $('.slider__arrows'),
+    nextArrow: $('.nxt-btn'),
+    prevArrow: $('.prev-btn'),
+    draggable: false,
+})
+
+// '<button type="button" class="slick-next nxt-btn"></button>'
