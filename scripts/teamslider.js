6d93be7fb41805a888b1a9d8330a23df76414353
shishka <benz.hokobyan@gmail.com>
2024-08-02 00:14:13 +0300

    Add files via upload

--- scripts/teamslider.js
+++ scripts/teamslider.js
@@ -1,10 +1,18 @@
-$('.team__slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    appendArrows: $('.slider__arrows'),
-    nextArrow: $('.nxt-btn'),
-    prevArrow: $('.prev-btn'),
-    draggable: false,
-})
+$(function(){ 
+   $('.team__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        appendArrows: $('.slider__arrows'),
+        nextArrow: $('.nxt-btn'),
+        prevArrow: $('.prev-btn'),
+        draggable: false,
+    })
 
-// '<button type="button" class="slick-next nxt-btn"></button>'
+    $('.testim__slider').slick({
+        slidesToShow: 1,
+        nextArrow: $('.nxt-btn-tesim'),
+        prevArrow: $('.prev-btn-tesim'),
+        dots: true,
+        appendDots: $('.testim__slider-dots'),
+    })
+})
